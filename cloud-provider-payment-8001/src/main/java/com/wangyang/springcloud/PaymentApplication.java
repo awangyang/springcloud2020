322c90552abe27d14035708488abcf7ea18e8a0c
wangyang <awangyang1198@163.com>
2020-06-17 18:26:11 +0800

    init

---
+++ cloud-provider-payment-8001/src/main/java/com/wangyang/springcloud/PaymentApplication.java
@@ -0,0 +1,16 @@
+package com.wangyang.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangyang
+ * @date 2020/06/17
+ */
+@SpringBootApplication
+public class PaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentApplication.class);
+    }
+}
